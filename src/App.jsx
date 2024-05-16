113591d4619c1cc2b9ad4b7e80a6b4f7d01e7958
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-05-16 13:57:34 -0600

    created react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/about' element={<h1>MyApp</h1>} />)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='jobs' element={<JobsPage />} />
+    </Route>
+  )
 );
 
 const App = () => {
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 };
 
 export default App;
